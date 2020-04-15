cec203240a5706ff2cb331ba51044900d90ffbf6
Ryan Duff <ryanduff613@gmail.com>
2020-04-15 00:11:51 -0700

    create prototype method for Entry that returns number of vowels in string

--- src/main.js
+++ src/main.js
@@ -7,6 +7,20 @@ Entry.prototype.totalWords = function () {
   return this.userText.split(' ').length
 }
 
+Entry.prototype.totalVowels = function() {
+  var totalVowels = 0;
+  var string = this.userText.toLowerCase();
+  for (var index = 0; index < this.userText.length; index++){
+    if (this.userText.charAt(index) === 'a' || this.userText.charAt(index) === 'e' || this.userText.charAt(index) === 'i' || this.userText.charAt(index) === 'o' || this.userText.charAt(index) === 'u')
+      totalVowels += 1;
+  }
+  return totalVowels;
+}
+
+Entry.prototype.totalConsonants = function() {
+
+}
+
 $(document).ready(function(){
   $("#userForm").submit(function(event){
     event.preventDefault();
@@ -14,7 +28,8 @@ $(document).ready(function(){
       var userText = $('#userText').val();
       var entry = new Entry(userName, userText);
       $("#wordCount").text(entry.totalWords());
-
+      $('#totalVowels').text(entry.totalVowels());
+      $('#totalConsonants').text(entry.totalConsonants());
 
   });
 });
