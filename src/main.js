7ad3991ff23be6defb334565be98d07dce479da7
Ryan Duff <ryanduff613@gmail.com>
2020-04-14 23:20:45 -0700

    front end working

--- src/main.js
+++ src/main.js
@@ -2,3 +2,19 @@ function Entry (userName, userText) {
   this.userName = userName;
   this.userText = userText;
 }
+
+Entry.prototype.totalWords = function () {
+  return this.userText.split(' ').length
+}
+
+$(document).ready(function(){
+  $("#userForm").submit(function(event){
+    event.preventDefault();
+      var userName = $('#userName').val();
+      var userText = $('#userText').val();
+      var entry = new Entry(userName, userText);
+      $("#wordCount").text(entry.totalWords());
+
+
+  });
+});
